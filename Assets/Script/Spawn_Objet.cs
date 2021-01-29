f964d3f48be639b86f161d961a394f2d53a0f3f1
Valentin Jacoberger <valbourgeois88@gmail.com>
2021-01-29 20:18:02 +0100

    Ajout du spawner + script spawner

---
+++ Assets/Script/Spawn_Objet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Objet : MonoBehaviour
+{
+    public GameObject[] objets;
+    public Transform spawner;
+    public float spawnRate = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnMethod", 1.0f, spawnRate);
+    }
+
+    void SpawnMethod()
+    {
+        Instantiate(objets[Random.Range(0, objets.Length)], spawner);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+} 
