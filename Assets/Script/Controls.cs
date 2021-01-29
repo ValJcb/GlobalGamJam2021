a26f4cbd085b8945a2befc2ff377f292abe9c3f9
Kibok <antoinespam@protonmail.com>
2021-01-29 19:31:47 +0100

    Ajouts scripts

---
+++ Assets/Script/Controls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragdrop : MonoBehaviour
+{
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
