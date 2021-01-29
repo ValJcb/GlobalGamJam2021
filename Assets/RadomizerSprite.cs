54983da4a5a1d0caf948684ee13139fc0d6417a3
Kibok <antoinespam@protonmail.com>
2021-01-29 22:49:36 +0100

    Spawner random d'objets

---
+++ Assets/RadomizerSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadomizerSprite : MonoBehaviour
+
+{
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, sprites.Length);
+        SpriteRenderer spriteR = GetComponent<SpriteRenderer>();
+        spriteR.sprite = sprites[rand];
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
