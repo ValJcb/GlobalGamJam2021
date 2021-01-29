c1be1a5027f8539dcd2ded752a88717573a02075
Antoine Lefevre <antoine.lefevre7@etu.univ-lorraine.fr>
2021-01-29 20:52:49 +0100

    Script trigger client v2

--- Assets/Script/ValidationClient.cs
+++ Assets/Script/ValidationClient.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ValidationClient : MonoBehaviour
 {
-    public GameObject objet;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (gameObject.CompareTag("Objet"))
