6b00538756486f34a3b6ee09eae74b94e8887df3
Kibok <antoinespam@protonmail.com>
2021-01-29 21:25:39 +0100

    Réparation item préfab

--- Assets/Script/Spawn_Objet.cs
+++ Assets/Script/Spawn_Objet.cs
@@ -10,6 +10,7 @@ public class Spawn_Objet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("ffef");
         InvokeRepeating("SpawnMethod", 1.0f, spawnRate);
     }
 
