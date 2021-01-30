c54eb3ba6b22d292d22675daa6c664efe5162293
Kibok <antoinespam@protonmail.com>
2021-01-30 11:23:24 +0100

    Random position spawn objet

--- Assets/Script/RadomizerSprite.cs
+++ Assets/Script/RadomizerSprite.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class RadomizerSprite : MonoBehaviour
 
 {
+    public float variationSpawn = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (gameObject.tag != "Client")
+        {
+            Transform spawn = this.transform;
+            spawn.position = new Vector3(spawn.position.x + Random.Range(-variationSpawn, variationSpawn), spawn.position.y, spawn.position.z);
+
+        }
+
         Sprite[] sprites = Resources.LoadAll<Sprite>("Objets") as Sprite[];
         int rand = Random.Range(0, sprites.Length);
         SpriteRenderer spriteR = GetComponent<SpriteRenderer>();
         spriteR.sprite = sprites[rand];
         if (gameObject.tag != "Client")
-        gameObject.AddComponent<PolygonCollider2D>();
+            gameObject.AddComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
