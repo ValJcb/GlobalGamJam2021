9919538e0737fa8cbcf4b6ff3b673c2b443857eb
Valentin Jacoberger <valbourgeois88@gmail.com>
2021-01-30 12:01:29 +0100

    Ajout du Timer

---
+++ Assets/Script/TimerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+
+    private float elapsedTime;
+
+
+    private void Start()
+    {
+        timeCounter.text = "Time: 00:00.00";
+        timerGoing = true;
+        elapsedTime = 0f;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
+    }
+}
