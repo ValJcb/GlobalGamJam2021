8eb854cf9c72699d096c7dcb1ceacfa337318457
Valentin Jacoberger <valbourgeois88@gmail.com>
2021-01-30 09:39:58 +0100

    Ajout Scène Menu + Menu principal

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Main_Scene");
     }
 
     // Update is called once per frame
-    void Update()
+    public void Quitter()
     {
-        
+        Application.Quit();
     }
 }
